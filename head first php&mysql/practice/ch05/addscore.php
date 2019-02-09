644014f3227b7ca0e4db07dae23975780c0480d9
cmoeryoung <cmoer.young@outlook.com>
2019-02-10 01:25:56 +0800

    ch05

--- head first php&mysql/practice/ch05/addscore.php
+++ head first php&mysql/practice/ch05/addscore.php
@@ -41,11 +41,15 @@
     }
     ?>
     <hr>
-    <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <form enctype="multipart/form-data" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+        <input type="hidden" name="MAX_FILE_SIZE" value="32768">
         <label for="name">Name:</label>
         <input type="text" id="name" name="name" value="<?php if (!empty($name)) echo $name; ?>"><br>
         <label for="score">Score:</label>
         <input type="text" id="score" name="score" value="<?php if (!empty($score)) echo $score; ?>">
+        <br>
+        <label for="screenshot">Screen shot:</label>
+        <input type="file" id="screenshot" name="screenshot">
         <hr>
         <input type="submit" value="Add" name="submit">
     </form>
--- head first php&mysql/practice/ch05/index.php
+++ head first php&mysql/practice/ch05/index.php
@@ -28,7 +28,12 @@
         echo '<tr><td class=""scoreinfo">';
         echo '<span class="score">'.$row['score'].'</span><br>';
         echo '<strong>Name:</strong>'.$row['name'].'<br>';
-        echo '<strong>Date:</strong>'.$row['date'].'</td></tr>';
+        echo '<strong>Date:</strong>'.$row['date'].'</td>';
+        if (is_file($row['screenshot']) && filesize($row['screenshot']) > 0){
+            echo '<td><img src="'.$row['screenshot'].'" alt="Score image"></td></tr>';
+        }else{
+            echo '<td><img src="unverified.gif" alt="Unverified score"></td></tr>';
+        }
     }
     echo '</table>';
 
